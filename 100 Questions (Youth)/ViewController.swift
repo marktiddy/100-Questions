444cc68e6c4156934994ff825b23e37d19b4bd39
Mark Tiddy <marktiddy@gmail.com>
2018-05-18 12:05:27 +0100

    Initial Commit

---
+++ 100 Questions (Youth)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  100 Questions (Youth)
+//
+//  Created by Developer on 18/05/2018.
+//  Copyright © 2018 Mark Tiddy Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
